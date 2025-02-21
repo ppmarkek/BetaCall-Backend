c130a334c33b47e8204e8e735ec7517b4d3f58c2
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2025-02-21 16:39:41 +0200

    fix: connect

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
